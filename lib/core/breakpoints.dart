d68e02180fdbf830a5ec83b7cb1b4e7d89ca673a
Ammar Montaser <ammarmontaser@icloud.com>
2024-04-27 21:24:44 +0300

    added breakpoints file to handle responsiveness.

---
+++ lib/core/breakpoints.dart
@@ -0,0 +1,7 @@
+class breakpoints {
+  static const smallMobileBP = 320;
+  static const mobileBP = 480;
+  static const tabletBP = 768;
+  static const desktopBP = 992;
+  static const largeDesktopBP = 992;
+}
