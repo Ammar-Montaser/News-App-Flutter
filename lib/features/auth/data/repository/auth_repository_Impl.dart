01bb202e83efcf9e4dc52f7fcad3646db3a61142
Ammar Montaser <ammarmontaser@icloud.com>
2024-04-30 22:51:15 +0300

    added sign up implementation in the AuthRepositoryImpl file

--- lib/features/auth/data/repository/auth_repository_Impl.dart
+++ lib/features/auth/data/repository/auth_repository_Impl.dart
@@ -1,8 +1,14 @@
+import 'package:articles_app/core/failure/exception.dart';
 import 'package:articles_app/core/failure/failure.dart';
+import 'package:articles_app/features/auth/data/datas_sources/auth_remote_db.dart';
 import 'package:articles_app/features/auth/domain/repository/auth_repository.dart';
-import 'package:fpdart/src/either.dart';
+import 'package:fpdart/fpdart.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDB authRemoteDB;
+
+  const AuthRepositoryImpl({required this.authRemoteDB});
+
   @override
   Future<Either<Failure, String>> SignInWithEmail(
       {required String email, required String password}) {
@@ -12,8 +18,15 @@ class AuthRepositoryImpl implements AuthRepository {
 
   @override
   Future<Either<Failure, String>> SignUpWithEmail(
-      {required String name, required String email, required String password}) {
-    // TODO: implement SignUpWithEmail
-    throw UnimplementedError();
+      {required String name,
+      required String email,
+      required String password}) async {
+    try {
+      final userID = await authRemoteDB.SignUpWithEmail(
+          name: name, email: email, password: password);
+      return right(userID);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
   }
 }
