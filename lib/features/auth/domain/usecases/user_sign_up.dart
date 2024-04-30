c4c100957080991a5bfb4829faef6bc41c1707b0
Ammar Montaser <ammarmontaser@icloud.com>
2024-04-30 23:20:47 +0300

    generated bloc files, created use case interface(core) and created Signup usecase (domain)

---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,23 @@
+import 'package:articles_app/core/failure/failure.dart';
+import 'package:articles_app/core/usecase/usecases.dart';
+import 'package:articles_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UserSignUp implements Usecase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+  const UserSignUp({required this.authRepository});
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.SignUpWithEmail(
+        name: params.name, email: params.email, password: params.password);
+  }
+}
+
+class UserSignUpParams {
+  final String name;
+  final String email;
+  final String password;
+
+  UserSignUpParams(
+      {required this.name, required this.email, required this.password});
+}
