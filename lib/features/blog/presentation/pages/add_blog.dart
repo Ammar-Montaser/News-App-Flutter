2e7f3c91a7954b7eb4e2b5e540dbc71e3860c848
Ammar Montaser <me@ammarmontaser.com>
2024-05-10 00:00:27 +0300

    added image picker to add blog screen + created blog entity and blog model

--- lib/features/blog/presentation/pages/add_blog.dart
+++ lib/features/blog/presentation/pages/add_blog.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:articles_app/core/theme/app_pallete.dart';
+import 'package:articles_app/core/utils/pick_image.dart';
 import 'package:articles_app/features/blog/presentation/widgets/blog_editor.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/cupertino.dart';
@@ -17,6 +20,17 @@ class _AddBlogState extends State<AddBlog> {
   final titleController = TextEditingController();
   final contentController = TextEditingController();
   List<String> selectedChips = [];
+  File? image;
+
+  void selectImage() async {
+    final pickedImage = await pickImage();
+    if (pickedImage != null) {
+      setState(() {
+        image = pickedImage;
+      });
+    }
+  }
+
   @override
   void dispose() {
     super.dispose();
@@ -41,39 +55,55 @@ class _AddBlogState extends State<AddBlog> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              GestureDetector(
-                child: DottedBorder(
-                  radius: Radius.circular(10),
-                  borderType: BorderType.RRect,
-                  strokeCap: StrokeCap.round,
-                  dashPattern: [10, 4],
-                  strokeWidth: 2,
-                  padding: EdgeInsets.all(10),
-                  child: Container(
-                    height: 150,
-                    width: double.infinity,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.folder_open,
-                          size: 50,
+              image == null
+                  ? GestureDetector(
+                      onTap: selectImage,
+                      child: DottedBorder(
+                        radius: Radius.circular(10),
+                        borderType: BorderType.RRect,
+                        strokeCap: StrokeCap.round,
+                        dashPattern: [10, 4],
+                        strokeWidth: 2,
+                        padding: EdgeInsets.all(10),
+                        child: Container(
+                          height: 150,
+                          width: double.infinity,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 50,
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                "Select an Image",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
                         ),
-                        SizedBox(
-                          height: 15,
+                      ),
+                    )
+                  : GestureDetector(
+                      onTap: selectImage,
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 150,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            image!,
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                        Text(
-                          "Select an Image",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .copyWith(fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-              ),
               SizedBox(
                 height: 15,
               ),
