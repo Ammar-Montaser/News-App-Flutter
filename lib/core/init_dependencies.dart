c8daa9cc4f3ee3bdf17754eb99c939ca83180e7f
Ammar Montaser <me@ammarmontaser.com>
2024-05-17 18:49:58 +0300

    modified dependencies to add connection checker services

--- lib/core/init_dependencies.dart
+++ lib/core/init_dependencies.dart
@@ -1,4 +1,5 @@
 import 'package:articles_app/core/common/cubits/app_user/app_user_cubit.dart';
+import 'package:articles_app/core/network/connection_checker.dart';
 import 'package:articles_app/core/secrets/app_secrets.dart';
 import 'package:articles_app/features/auth/data/datas_sources/auth_remote_db.dart';
 import 'package:articles_app/features/auth/data/repository/auth_repository_Impl.dart';
@@ -14,6 +15,7 @@ import 'package:articles_app/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:articles_app/features/blog/domain/usecases/upload_blog.dart';
 import 'package:articles_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final serviceLocator = GetIt.instance;
@@ -24,6 +26,9 @@ Future<void> initDependencies() async {
   final supabase = await Supabase.initialize(
       url: AppSecrets.url, anonKey: AppSecrets.anonKey);
   serviceLocator.registerLazySingleton(() => supabase.client);
+  serviceLocator.registerFactory(() => InternetConnection());
+  serviceLocator.registerFactory<ConnectionChecker>(
+      () => ConnectionCheckerImpl(internetConnection: serviceLocator()));
 }
 
 void _initAuth() {
@@ -31,8 +36,8 @@ void _initAuth() {
     ..registerFactory<AuthRemoteDB>(() => AuthRemoteDBImpl(
           serviceLocator(),
         ))
-    ..registerFactory<AuthRepository>(
-        () => AuthRepositoryImpl(authRemoteDB: serviceLocator()))
+    ..registerFactory<AuthRepository>(() => AuthRepositoryImpl(
+        authRemoteDB: serviceLocator(), connectionChecker: serviceLocator()))
     ..registerFactory(() => UserSignUp(authRepository: serviceLocator()))
     ..registerFactory(() => UserLogin(authRepository: serviceLocator()))
     ..registerFactory(() => CurrentUser(authRepository: serviceLocator()))
--- lib/features/auth/data/repository/auth_repository_Impl.dart
+++ lib/features/auth/data/repository/auth_repository_Impl.dart
@@ -1,15 +1,20 @@
 import 'package:articles_app/core/failure/exception.dart';
 import 'package:articles_app/core/failure/failure.dart';
+import 'package:articles_app/core/network/connection_checker.dart';
 import 'package:articles_app/features/auth/data/datas_sources/auth_remote_db.dart';
 import 'package:articles_app/core/common/entities/user.dart';
+import 'package:articles_app/features/auth/data/models/userModel.dart';
 import 'package:articles_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:supabase_flutter/supabase_flutter.dart' as supabase;
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDB authRemoteDB;
-
-  const AuthRepositoryImpl({required this.authRemoteDB});
+  final ConnectionChecker connectionChecker;
+  const AuthRepositoryImpl({
+    required this.authRemoteDB,
+    required this.connectionChecker,
+  });
 
   @override
   Future<Either<Failure, User>> SignInWithEmail(
@@ -29,8 +34,12 @@ class AuthRepositoryImpl implements AuthRepository {
 
   Future<Either<Failure, User>> _getUser(Future<User> Function() fn) async {
     try {
-      final user = await fn();
-      return right(user);
+      if (await connectionChecker.isConnected) {
+        final user = await fn();
+        return right(user);
+      } else {
+        return left(Failure("No internet connection!"));
+      }
     } on supabase.AuthException catch (e) {
       return left(Failure(e.message));
     } on ServerException catch (e) {
@@ -41,6 +50,15 @@ class AuthRepositoryImpl implements AuthRepository {
   @override
   Future<Either<Failure, User>> currentUser() async {
     try {
+      if (!await connectionChecker.isConnected) {
+        final session = authRemoteDB.currentUserSession;
+        if (session == null) {
+          return left(Failure("User not logged in"));
+        }
+        return right(UserModel(
+            id: session.user.id, name: "", email: session.user.email ?? ""));
+      }
+
       final res = await authRemoteDB.getCurrentUserData();
       if (res != null) {
         return right(res);
